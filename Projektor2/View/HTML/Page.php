ef9757d97cad4d2563e491367f96dba76a9d7368
Instalace <Instalace@Home-PC>
2015-02-16 23:13:37 +0100

    20150216 23:13

---
+++ Projektor2/View/HTML/Page.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Layout
+ *
+ * @author pes2704
+ */
+class Projektor2_View_HTML_Page extends Framework_View_Abstract {
+    public function render() {
+        $this->parts[] = '<!DOCTYPE html>';
+        $this->parts[] = '<html class="no-js">';
+            $this->parts[] = $this->context['headControllerResult'];
+            $this->parts[] = '<body onload = "Zobraz_pdf();">';     
+                $this->parts[] = $this->context['headerControllerResult'];
+                $this->parts[] = $this->context['containerControllerResult'];
+                $this->parts[] = $this->context['footerControllerResult'];
+            $this->parts[] = '</body>';
+        $this->parts[] = '</html>';
+        return $this;
+    }
+}
